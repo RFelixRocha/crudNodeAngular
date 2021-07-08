105ca0add099e4f23f89f0944cedf95edd019ee7
FelixRocha <felix@jerab.com.br>
2021-07-08 12:40:32 -0400

    Correção de bug na migration.

---
+++ src/database/migrations/20210708143646-create-users.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    return queryInterface.createTable('users',{
+
+      id: {
+        type:Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      foto: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      escolaridade: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+
+    })
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return await queryInterface.dropTable('users');
+  }
+};
