392824bc7ddd12c1ed013aad76c08038be4f8ca5
RFelixRocha <felixrochaeso@gmail.com>
2021-07-11 01:42:46 -0400

    Alteração na interface do user

--- frontend/src/app/interfaces/user.ts
+++ frontend/src/app/interfaces/user.ts
@@ -1,9 +1,9 @@
 export interface User {
     id: number;
     name: string;
-    idade: number;
+    idade: any;
     email: string;
     foto_key: string;
     foto_url: string;
-    escolaridade: number;
+    escolaridade: any;
 }
